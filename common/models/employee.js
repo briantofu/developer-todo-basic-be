e0c419505226f14ce57b93831e935a5873ff9c36
Greatus <adriancyago@gmail.com>
2018-03-30 14:43:07 +0800

    Disable unnecessary end points

--- common/models/employee.js
+++ common/models/employee.js
@@ -1,5 +1,22 @@
 'use strict';
 
 module.exports = function(Employee) {
+    Employee.disableRemoteMethodByName('exists');
+    Employee.disableRemoteMethodByName('patchOrCreate');
+    Employee.disableRemoteMethodByName('createChangeStream');
+    Employee.disableRemoteMethodByName('prototype.patchAttributes');
+    Employee.disableRemoteMethodByName('updateAll');
+    Employee.disableRemoteMethodByName('replaceOrCreate');
+    Employee.disableRemoteMethodByName('findOne');
+    Employee.disableRemoteMethodByName('upsertWithWhere');
 
+    Employee.disableRemoteMethodByName('prototype.__create__assignedTasks');
+    Employee.disableRemoteMethodByName('prototype.__delete__createdTasks');
+    Employee.disableRemoteMethodByName('prototype.__delete__assignedTasks');
+    Employee.disableRemoteMethodByName('prototype.__destroyById__createdTasks');
+    Employee.disableRemoteMethodByName('prototype.__destroyById__assignedTasks');
+    Employee.disableRemoteMethodByName('prototype.__updateById__createdTasks');
+    Employee.disableRemoteMethodByName('prototype.__updateById__assignedTasks'); 
+    Employee.disableRemoteMethodByName('prototype.__findById__createdTasks');
+    Employee.disableRemoteMethodByName('prototype.__findById__assignedTasks');
 };
--- common/models/log.js
+++ common/models/log.js
@@ -1,5 +1,20 @@
 'use strict';
 
 module.exports = function(Log) {
+    Log.disableRemoteMethodByName('exists');
+    Log.disableRemoteMethodByName('patchOrCreate');
+    Log.disableRemoteMethodByName('createChangeStream');
+    Log.disableRemoteMethodByName('prototype.patchAttributes');
+    Log.disableRemoteMethodByName('updateAll');
+    Log.disableRemoteMethodByName('replaceOrCreate');
+    Log.disableRemoteMethodByName('upsertWithWhere');
+    Log.disableRemoteMethodByName('create');
 
+    Log.disableRemoteMethodByName('find');
+    Log.disableRemoteMethodByName('findById');
+    Log.disableRemoteMethodByName('findOne');
+    Log.disableRemoteMethodByName('count');
+
+    Log.disableRemoteMethodByName('prototype.__create__task');
+    Log.disableRemoteMethodByName('prototype.__get__task');
 };
